1ccb9ab96385883cb8ecc6d4a2077e3896059a3f
Victor Bertoli <bertoli.victor91@gmai.com>
2025-12-16 14:40:07 -0300

    finalizar projeto

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,19 +24,12 @@ function escopoIMC (){
             showResult("Altura inválida, inserir um número MENOR que 2.80m", "obesidade-3");
             return;
         }
-        if(userIMC < 18.5) {
-            showResult(`Seu IMC é ${userIMC} (Abaixo do peso)`, "peso-atencao");
-        } else if(userIMC >= 18.5 && userIMC < 25) {
-            showResult(`Seu IMC é ${userIMC} (Peso normal)`, "peso-normal");
-        } else if(userIMC >= 25 && userIMC < 30) {
-            showResult(`Seu IMC é ${userIMC} (Sobrepeso)`, "peso-atencao");
-        } else if(userIMC >= 30 && userIMC < 35) {
-            showResult(`Seu IMC é ${userIMC} (Obesidade grau 1)`, "obesidade-1");
-        } else if(userIMC >= 35 && userIMC < 40) {
-            showResult(`Seu IMC é ${userIMC} (Obesidade grau 2)`, "obesidade-2");
-        } else {
-            showResult(`Seu IMC é ${userIMC} (Obesidade grau 3)`, "obesidade-3");
-        }
+        if(userIMC < 18.5) showResult(`Seu IMC é ${userIMC} (Abaixo do peso)`, "peso-atencao");
+        if(userIMC >= 18.5 && userIMC < 25) return showResult(`Seu IMC é ${userIMC} (Peso normal)`, "peso-normal");
+        if(userIMC >= 25 && userIMC < 30) return showResult(`Seu IMC é ${userIMC} (Sobrepeso)`, "peso-atencao");
+        if(userIMC >= 30 && userIMC < 35) return showResult(`Seu IMC é ${userIMC} (Obesidade grau 1)`, "obesidade-1");
+        if(userIMC >= 35 && userIMC < 40) return showResult(`Seu IMC é ${userIMC} (Obesidade grau 2)`, "obesidade-2");
+        if (userIMC >= 40) return showResult(`Seu IMC é ${userIMC} (Obesidade grau 3)`, "obesidade-3");
     }
     function showResult(message, className) {
         const oldResult = container.querySelector(".resultado");
